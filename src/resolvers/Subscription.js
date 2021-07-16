e593d2ccc106887654ff2f25251e16c832eb0f79
DanielFryy <dgz1606@gmail.com>
2021-07-15 22:09:03 -0500

    remove basic count subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,15 +1,4 @@
 const Subscription = {
-  count: {
-    subscribe: (parent, args, ctx, info) => {
-      const { pubsub } = ctx;
-      let count = 0;
-      setInterval(() => {
-        count++;
-        pubsub.publish('count', { count });
-      }, 1000);
-      return pubsub.asyncIterator('count');
-    },
-  },
   comment: {
     subscribe: (parent, args, ctx, info) => {
       const { postId } = args;
